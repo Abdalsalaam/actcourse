fcfa08b9ef0ca58f578e29d7947b215c21ad283f
Abdalsalaam Halawa <abdalsalaam@outlook.com>
2024-08-28 16:36:21 +0300

    get categories and tags

--- single.php
+++ single.php
@@ -16,8 +16,24 @@ get_header( 'page' );
 	                if ( have_posts() ) : while ( have_posts() ) : the_post();
 		                ?>
 		                <?php the_post_thumbnail( 'post-thumbnail', array( 'class' => 'img-fluid services-img' ) ); ?>
+                    <div>
+                        <?php
+                        $categories = get_the_category();
+                        foreach ( $categories as $category ) {
+                            echo '<a style="padding: 0 12px;" href="' . get_category_link( $category ) . '">' . $category->name . '</a>';
+                        }
+                        ?>
+                    </div>
                         <h3><?php the_title();?></h3>
-                        <?php the_content();?>
+                        <?php the_content(); ?>
+		                <?php
+		                $tags = get_the_tags();
+                        if ( $tags && ! is_wp_error( $tags ) ) {
+	                        foreach ( $tags as $tag ) {
+		                        echo '<a style="padding: 0 12px;" href="' . get_tag_link( $tag ) . '">' . $tag->name . '</a>';
+	                        }
+                        }
+		                ?>
 	                <?php
 	                endwhile;
 	                endif;
